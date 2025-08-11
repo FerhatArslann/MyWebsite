7e51834dfd3419128ccd4077eeca033ab2125f7e
Ferhat Arslan <ferhat.arslanin@gmail.com>
2025-08-11 23:38:15 +0300

    fixed connection to db

--- scripts/server.js
+++ scripts/server.js
@@ -11,11 +11,9 @@ const ContactMessage = require('./models/ContactMessage');
 require('dotenv').config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 const app = express();
 app.use(cors()); // Enable Cross-Origin Resource Sharing
