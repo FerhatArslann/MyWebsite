173c757088b6a8280348724d8b1ba1c13dc82342
Ferhat Arslan <ferhat.arslanin@gmail.com>
2025-08-13 16:05:19 +0300

    Refactor project structure: removed some features and added json file for portfolio management.

--- scripts/models/Newsletter.js
+++
@@ -1,8 +0,0 @@
-// models/Newsletter.js
-
-const mongoose = require('mongoose');
-const NewsletterSchema = new mongoose.Schema({
-    email: String,
-    createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Newsletter', NewsletterSchema);
